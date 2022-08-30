3e21de1abbe8814e13485448f0bb131a4f4193dd
GastonRossum <94536177+GastonRossum@users.noreply.github.com>
2022-08-30 12:06:53 +0800

    Create selection.py
    采用随机数进行随机选号

---
+++ two-color ball/selection.py
@@ -0,0 +1,12 @@
+import random
+
+red1 = random.randint(1, 33)
+red2 = random.randint(1, 33)
+red3 = random.randint(1, 33)
+red4 = random.randint(1, 33)
+red5 = random.randint(1, 33)
+red6 = random.randint(1, 33)
+
+blue = random.randint(1,16)
+
+print(blue)     # 输出：篮球
